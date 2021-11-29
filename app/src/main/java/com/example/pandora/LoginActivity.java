07595c6983bd2427e71b1c308918b9c17c91e36a
FranciscoETSD <tadeo00704@gmail.com>
2021-11-29 11:40:06 -0400

    opcion de registro implementado

--- app/src/main/java/com/example/pandora/LoginActivity.java
+++ app/src/main/java/com/example/pandora/LoginActivity.java
@@ -3,12 +3,17 @@ package com.example.pandora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+public class  LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void goCreateAccount(View view) {
+        System.out.println("-----------> go to create Account");
     }
 }
